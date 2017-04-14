4329eb52542ecb544e0d8e6d92bc1345ceda6200
itriA30110 <itria30110@itri.org.tw>
2017-04-14 16:06:24 +0800

    update make_clean.sh

--- make_clean.sh
+++ make_clean.sh
@@ -1,3 +1,8 @@
 ## take this as replacement of make clean
 #rm -rf /opt/user/workspace/code/android-x86-lxc/out/target/product/x86/obj/lxc/
-rm ../../out/target/product/svmp/system/lib/liblxc.so
+###rm ../../out/target/product/svmp/system/lib/liblxc.so
+##rm $OUT/system/lib/liblxc.so
+###### Refer to http://stackoverflow.com/questions/19074431/how-to-do-an-android-mm-clean
+
+echo make clean-liblxc in TOPDIR
+echo or mm -B in modules dir
